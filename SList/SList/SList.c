00c637bf4e84808b3504ff6258588e8e16333aac
zhou-liurong <19551230598@163.com>
2024-04-17 15:05:58 +0800

    单向链表增删查改各项功能

--- SList/SList/SList.c
+++ SList/SList/SList.c
@@ -1,5 +1,5 @@
 #include"SList.h"
-
+#include"vld.h"
 void SLTPrint(SLTNode* phead)
 {
 	SLTNode* pcur = phead;
@@ -23,6 +23,8 @@ SLTNode* SLTBuyNode(SLTDataType x)
 	newnode->next = NULL;
 	return newnode;
 }
+
+//头部插入删除/尾部插入删除
 
 void SLTPushBack(SLTNode** pphead, SLTDataType x)
 {
@@ -62,7 +64,7 @@ void SLTPopBack(SLTNode** pphead)
 {
 	assert(pphead && *pphead);
 
-	if ((*pphead)->next)
+	if ((*pphead)->next == NULL)
 	{
 		free(*pphead);
 		*pphead = NULL;
@@ -86,17 +88,103 @@ void SLTPopBack(SLTNode** pphead)
 void SLTPopFront(SLTNode** pphead)
 {
 	assert(pphead && *pphead);
-	SLTNode* next = NULL;
-	if((*pphead)->next)
+
+	SLTNode* next = (*pphead)->next;
+	free(*pphead);
+	*pphead = next;
+}
+
+//查找
+SLTNode* SLTFind(SLTNode* phead, SLTDataType x)
+{
+	assert(phead);
+	SLTNode* pcur = phead;
+	while(pcur)
 	{
-		next = (*pphead)->next;
-		free(*pphead);
-		*pphead = next;
+		if (pcur->data == x)
+		{
+			return pcur;
+		}
+		pcur = pcur->next;
+	}
+	return NULL;
+}
+
+//在指定位置之前插入数据
+void SLTInsert(SLTNode** pphead, SLTNode* pos, SLTDataType x)
+{
+	assert(pphead && *pphead);
+	assert(pos);
+	SLTNode* newnode = SLTBuyNode(x);
+	SLTNode* pcur = *pphead;
+	if (pos == *pphead)
+	{
+		SLTPushFront(pphead, x);
 	}
 	else
 	{
-		next = (*pphead)->next;
-		free((*pphead)->next);
-		(*pphead)->next = next->next;
+		while (pcur->next != pos)
+		{
+			pcur = pcur->next;
+		}
+		newnode->next = pcur->next;
+		pcur->next = newnode;
 	}
+
+}
+
+//在指定位置之后插入数据
+void SLTInsertAfter(SLTNode* pos, SLTDataType x)
+{
+	assert(pos);
+	SLTNode*  newnode = SLTBuyNode(x);
+	newnode->next = pos->next;
+	pos->next = newnode;
+
+}
+
+//删除pos节点
+void SLTErase(SLTNode** pphead, SLTNode* pos)
+{
+	assert(pphead && *pphead);
+	assert(pos);
+	SLTNode* pcur = *pphead;
+	if(pos == *pphead)
+	{
+		SLTPopFront(pphead);
+	}
+	else
+	{
+		while (pcur->next != pos)
+		{
+			pcur = pcur->next;
+		}
+		pcur->next = pos->next;
+		free(pos);
+		pos = NULL;
+	}
+}
+
+//删除pos之后的节点
+void SLTEraseAfter(SLTNode* pos)
+{
+	assert(pos && pos->next);
+	SLTNode* pcur = pos->next->next;
+	free(pos->next);
+	pos->next = pcur;
+}
+
+//销毁链表
+void SListDesTroy(SLTNode** pphead)
+{
+	assert(pphead && *pphead);
+	SLTNode* pcur = *pphead;
+	SLTNode* prev = *pphead;
+	while(pcur)
+	{
+		prev = pcur;
+		pcur = pcur->next;
+		free(prev);
+	}
+	*pphead = prev = pcur = NULL;
 }
--- SList/SList/test.c
+++ SList/SList/test.c
@@ -1,6 +1,6 @@
 #include"SList.h"
 
-void test(void)
+void test01(void)
 {
 	SLTNode* phead = NULL;
 	SLTPrint(phead);
@@ -19,9 +19,9 @@ void test(void)
 	SLTPopFront(&phead);
 	SLTPrint(phead);
 	SLTPopBack(&phead);
-	SLTPrint(phead);
-	SLTPopBack(&phead);
-	SLTPrint(phead);
+	//SLTPrint(phead);
+	//SLTPopBack(&phead);
+	//SLTPrint(phead);
 	//SLTPopBack(&phead);
 	//SLTPrint(phead);
 	//SLTPopBack(&phead);
@@ -30,12 +30,78 @@ void test(void)
 	//SLTPrint(phead);
 }
 
+void test02(void)
+{
+	SLTNode* phead = NULL;
+	SLTPrint(phead);
+	SLTPushFront(&phead, 1);
+	SLTPrint(phead);
+	SLTPushFront(&phead, 2);
+	SLTPrint(phead);
+	SLTPushFront(&phead, 3);
+	SLTPrint(phead);
+	SLTPushFront(&phead, 4);
+	SLTPrint(phead);
+	//SLTNode* ret = SLTFind(phead, 1);
+	//if(ret == NULL)
+	//{
+	//	printf("找不到\n");
+	//}
+	//else
+	//{
+	//	printf("找到了\n");
+	//}
+	//SLTInsert(&phead, ret, 6);
+	//SLTPrint(phead);
+	//ret = SLTFind(phead, 4);
+	//SLTInsert(&phead, ret, 9);
+	//SLTPrint(phead);
+	//SLTErase(&phead,ret);
+	//SLTPrint(phead);
+	//ret = SLTFind(phead, 4);
+	//SLTErase(&phead, ret);
+	//SLTPrint(phead);
+	//ret = SLTFind(phead, 3);
+	//SLTErase(&phead, ret);
+	//SLTPrint(phead);
+	//ret = SLTFind(phead, 2);
+	//SLTErase(&phead, ret);
+	//SLTPrint(phead);
+	//SListDesTroy(&phead);
+	//SLTPrint(phead);
+	SLTPopBack(&phead);
+	SLTPrint(phead);
+}
 
+void test03(void)
+{
+	SLTNode* phead = NULL;
+	SLTPrint(phead);
+	SLTPushFront(&phead, 1);
+	SLTPrint(phead);
+	SLTPushFront(&phead, 2);
+	SLTPrint(phead);
+	SLTPushFront(&phead, 3);
+	SLTPrint(phead);
+	SLTPushFront(&phead, 4);
+	SLTPrint(phead);
+	//SLTNode* ret = SLTFind(phead, 1);
+	//SLTInsertAfter(ret, 6);
+	//SLTPrint(phead);
+	//SLTPopBack(&phead);
+	//SLTPrint(phead);
+	//SLTNode* ret = SLTFind(phead, 2);
+	//SLTEraseAfter(ret);
+	//SLTPrint(phead);
+	SListDesTroy(&phead);
+}
 
 int main()
 {
 
-	test();
+	//test01();
+	//test02();
+	test03();
 
 	return 0;
 }
