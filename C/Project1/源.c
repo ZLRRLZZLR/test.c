766e0e1a59648294bba1b2470967e6a12dcde47d
zhou-liurong <19551230598@163.com>
2024-04-29 22:55:14 +0800

    日常练习

--- C/Project1/源.c
+++ C/Project1/源.c
@@ -3,7 +3,64 @@
 #include<string.h>
 
 
+//打印1到最大的n位数
+//描述
+//输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+//1. 用返回一个整数列表来代替打印
+//2. n 为正整数，0 < n <= 5
+//
+///**
+// * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+// *
+// *
+// * @param n int整型 最大位数
+// * @return int整型一维数组
+// * @return int* returnSize 返回数组行数
+// */
+//static int arr[100000];
+//int* printNumbers(int n, int* returnSize) {
+//	int a = 10;
+//	while (--n)
+//	{
+//		a *= 10;
+//	}
+//	for (int i = 1; i < a; i++)
+//	{
+//		arr[i - 1] = i;
+//	}
+//	*returnSize = a - 1;
+//	return arr;
+//}
 
+//青蛙跳台阶：第一跳只有跳一阶或两阶，然后以此为新起点
+// #include <stdio.h>
+//
+//int main() {
+//	int n;
+//	int f1 = 1;
+//	int f2 = 2;
+//	int f = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		if (n > 2)
+//		{
+//			n = n - 2;
+//			while (n--)
+//			{
+//				f = f1 + f2;
+//				f1 = f2;
+//				f2 = f;
+//			}
+//			printf("%d\n", f);
+//		}
+//		else
+//		{
+//			printf("%d", n);
+//		}
+//
+//	}
+//	return 0;
+//}
 
 //相交链表
 ///**
