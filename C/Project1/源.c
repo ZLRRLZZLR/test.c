e773ac4195bd204d4571e0a320182e903e69edce
zhou-liurong <19551230598@163.com>
2024-05-06 22:30:59 +0800

    无

--- C/Project1/源.c
+++ C/Project1/源.c
@@ -111,6 +111,230 @@
 //	}
 //	return false;
 //}
+// //相交链表
+///**
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     struct ListNode *next;
+// * };
+// */
+//struct ListNode* getIntersectionNode(struct ListNode* headA, struct ListNode* headB) {
+//	int len1 = 0, len2 = 0;
+//	struct ListNode* HeadA = headA, * HeadB = headB;
+//	while (HeadA)
+//	{
+//		HeadA = HeadA->next;
+//		len1++;
+//	}
+//	while (HeadB)
+//	{
+//		HeadB = HeadB->next;
+//		len2++;
+//	}
+//	int gap = abs(len1 - len2);
+//	struct ListNode* longer = headA, * shoter = headB;
+//	if (len2 > len1)
+//	{
+//		longer = headB;
+//		shoter = headA;
+//	}
+//	while (gap--)
+//	{
+//		longer = longer->next;
+//	}
+//	while (longer && shoter)
+//	{
+//		if (longer == shoter)
+//		{
+//			return longer;
+//		}
+//		longer = longer->next;
+//		shoter = shoter->next;
+//	}
+//	return NULL;
+//}
+
+//链表的回文结构
+///*
+//struct ListNode {
+//	int val;
+//	struct ListNode *next;
+//	ListNode(int x) : val(x), next(NULL) {}
+//};*/
+//ListNode* Mid(ListNode* head)
+//{
+//	struct ListNode* slow = head, * fast = head;
+//	while (fast && fast->next)
+//	{
+//		slow = slow->next;
+//		fast = fast->next->next;
+//	}
+//	return slow;
+//}
+//ListNode* Reverse(ListNode* head)
+//{
+//	struct ListNode* cur = head, * newhead = NULL;
+//	while (cur)
+//	{
+//		struct ListNode* n = cur->next;
+//		cur->next = newhead;
+//		newhead = cur;
+//		cur = n;
+//	}
+//	return newhead;
+//
+//}
+//class PalindromeList {
+//public:
+//	bool chkPalindrome(ListNode* A) {
+//		struct ListNode* mid = Mid(A);
+//		struct ListNode* Newhead = Reverse(mid);
+//		while (A && Newhead)
+//		{
+//			if (A->val != Newhead->val)
+//			{
+//				return false;
+//			}
+//			A = A->next;
+//			Newhead = Newhead->next;
+//		}
+//		return true;
+//	}
+//};
+
+//实现一种算法，找出单向链表中倒数第 k 个节点。返回该节点的值。
+//int kthToLast(struct ListNode* head, int k) {
+//	struct ListNode* slow = head, * fast = head;
+//	while (k--)
+//	{
+//		fast = fast->next;
+//	}
+//	while (fast)
+//	{
+//		fast = fast->next;
+//		slow = slow->next;
+//	}
+//	return slow->val;
+//}
+// ///**//合并两个有序链表
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     struct ListNode *next;
+// * };
+// */
+//typedef struct ListNode ListNode;
+//struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+//	if (list1 == NULL)
+//		return list2;
+//	if (list2 == NULL)
+//		return list1;
+//	ListNode* l1 = list1;
+//	ListNode* l2 = list2;
+//	ListNode* newhead = NULL, * newtail = NULL;
+//	while (l1 && l2)
+//	{
+//		if (l1->val < l2->val)
+//		{
+//			if (newhead == NULL)
+//			{
+//				newhead = newtail = l1;
+//			}
+//			else
+//			{
+//				newtail->next = l1;
+//				newtail = newtail->next;
+//			}
+//			l1 = l1->next;
+//		}
+//		else
+//		{
+//			if (newhead == NULL)
+//			{
+//				newhead = newtail = l2;
+//			}
+//			else
+//			{
+//				newtail->next = l2;
+//				newtail = newtail->next;
+//			}
+//			l2 = l2->next;
+//		}
+//	}
+//	if (l1 == NULL)
+//	{
+//		newtail->next = l2;
+//
+//
+//	}
+//	if (l2 == NULL)
+//	{
+//		newtail->next = l1;
+//
+//
+//	}
+//	return newhead;
+//}
+
+
+//约瑟夫问题环形链表
+///**
+// * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+// *
+// *
+// * @param n int整型
+// * @param m int整型
+// * @return int整型
+// */
+//typedef struct ListNode ListNode;
+//ListNode* buynode(int x)
+//{
+//	ListNode* next = (ListNode*)malloc(sizeof(ListNode));
+//	if (NULL == next)
+//	{
+//		exit(1);
+//	}
+//	next->val = x;
+//	next->next = NULL;
+//	return next;
+//}
+//ListNode* creatcricle(int n)
+//{
+//	ListNode* phead = buynode(1);
+//	ListNode* pcur = phead;
+//	for (int i = 2; i <= n; i++)
+//	{
+//		pcur->next = buynode(i);
+//		pcur = pcur->next;
+//	}
+//	pcur->next = phead;
+//	return pcur;
+//}
+//
+//int ysf(int n, int m) {
+//	ListNode* prev = creatcricle(n);
+//	ListNode* pcur = prev->next;
+//	int count = 1;
+//	while (pcur->next != pcur)
+//	{
+//		if (m == count)
+//		{
+//			prev->next = pcur->next;
+//			free(pcur);
+//			pcur = prev->next;
+//			count = 1;
+//		}
+//		else
+//		{
+//			prev = pcur;
+//			pcur = pcur->next;
+//			count++;
+//		}
+//
+//	}
+//	return pcur->val;
+//}
 //删除公共字符
 //#include <stdio.h>
 //#include<string.h>
@@ -393,6 +617,7 @@
 //	return arr;
 //}
 
+
 //青蛙跳台阶：第一跳只有跳一阶或两阶，然后以此为新起点
 // #include <stdio.h>
 //
@@ -423,112 +648,7 @@
 //	return 0;
 //}
 
-//相交链表
-///**
-// * Definition for singly-linked list.
-// * struct ListNode {
-// *     int val;
-// *     struct ListNode *next;
-// * };
-// */
-//struct ListNode* getIntersectionNode(struct ListNode* headA, struct ListNode* headB) {
-//	int len1 = 0, len2 = 0;
-//	struct ListNode* HeadA = headA, * HeadB = headB;
-//	while (HeadA)
-//	{
-//		HeadA = HeadA->next;
-//		len1++;
-//	}
-//	while (HeadB)
-//	{
-//		HeadB = HeadB->next;
-//		len2++;
-//	}
-//	int gap = abs(len1 - len2);
-//	struct ListNode* longer = headA, * shoter = headB;
-//	if (len2 > len1)
-//	{
-//		longer = headB;
-//		shoter = headA;
-//	}
-//	while (gap--)
-//	{
-//		longer = longer->next;
-//	}
-//	while (longer && shoter)
-//	{
-//		if (longer == shoter)
-//		{
-//			return longer;
-//		}
-//		longer = longer->next;
-//		shoter = shoter->next;
-//	}
-//	return NULL;
-//}
 
-//链表的回文结构
-///*
-//struct ListNode {
-//	int val;
-//	struct ListNode *next;
-//	ListNode(int x) : val(x), next(NULL) {}
-//};*/
-//ListNode* Mid(ListNode* head)
-//{
-//	struct ListNode* slow = head, * fast = head;
-//	while (fast && fast->next)
-//	{
-//		slow = slow->next;
-//		fast = fast->next->next;
-//	}
-//	return slow;
-//}
-//ListNode* Reverse(ListNode* head)
-//{
-//	struct ListNode* cur = head, * newhead = NULL;
-//	while (cur)
-//	{
-//		struct ListNode* n = cur->next;
-//		cur->next = newhead;
-//		newhead = cur;
-//		cur = n;
-//	}
-//	return newhead;
-//
-//}
-//class PalindromeList {
-//public:
-//	bool chkPalindrome(ListNode* A) {
-//		struct ListNode* mid = Mid(A);
-//		struct ListNode* Newhead = Reverse(mid);
-//		while (A && Newhead)
-//		{
-//			if (A->val != Newhead->val)
-//			{
-//				return false;
-//			}
-//			A = A->next;
-//			Newhead = Newhead->next;
-//		}
-//		return true;
-//	}
-//};
-
-//实现一种算法，找出单向链表中倒数第 k 个节点。返回该节点的值。
-//int kthToLast(struct ListNode* head, int k) {
-//	struct ListNode* slow = head, * fast = head;
-//	while (k--)
-//	{
-//		fast = fast->next;
-//	}
-//	while (fast)
-//	{
-//		fast = fast->next;
-//		slow = slow->next;
-//	}
-//	return slow->val;
-//}
 //数组nums包含从0到n的所有整数，但其中缺了一个。请编写代码找出那个缺失的整数。
 //int missingNumber(int* nums, int numsSize) {
 //	int num = 0;
@@ -746,124 +866,7 @@
 //	return 0;
 //}
 
-///**//合并两个有序链表
-// * Definition for singly-linked list.
-// * struct ListNode {
-// *     int val;
-// *     struct ListNode *next;
-// * };
-// */
-//typedef struct ListNode ListNode;
-//struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-//	if (list1 == NULL)
-//		return list2;
-//	if (list2 == NULL)
-//		return list1;
-//	ListNode* l1 = list1;
-//	ListNode* l2 = list2;
-//	ListNode* newhead = NULL, * newtail = NULL;
-//	while (l1 && l2)
-//	{
-//		if (l1->val < l2->val)
-//		{
-//			if (newhead == NULL)
-//			{
-//				newhead = newtail = l1;
-//			}
-//			else
-//			{
-//				newtail->next = l1;
-//				newtail = newtail->next;
-//			}
-//			l1 = l1->next;
-//		}
-//		else
-//		{
-//			if (newhead == NULL)
-//			{
-//				newhead = newtail = l2;
-//			}
-//			else
-//			{
-//				newtail->next = l2;
-//				newtail = newtail->next;
-//			}
-//			l2 = l2->next;
-//		}
-//	}
-//	if (l1 == NULL)
-//	{
-//		newtail->next = l2;
-//
-//
-//	}
-//	if (l2 == NULL)
-//	{
-//		newtail->next = l1;
-//
-//
-//	}
-//	return newhead;
-//}
 
-
-//约瑟夫问题环形链表
-///**
-// * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-// *
-// *
-// * @param n int整型
-// * @param m int整型
-// * @return int整型
-// */
-//typedef struct ListNode ListNode;
-//ListNode* buynode(int x)
-//{
-//	ListNode* next = (ListNode*)malloc(sizeof(ListNode));
-//	if (NULL == next)
-//	{
-//		exit(1);
-//	}
-//	next->val = x;
-//	next->next = NULL;
-//	return next;
-//}
-//ListNode* creatcricle(int n)
-//{
-//	ListNode* phead = buynode(1);
-//	ListNode* pcur = phead;
-//	for (int i = 2; i <= n; i++)
-//	{
-//		pcur->next = buynode(i);
-//		pcur = pcur->next;
-//	}
-//	pcur->next = phead;
-//	return pcur;
-//}
-//
-//int ysf(int n, int m) {
-//	ListNode* prev = creatcricle(n);
-//	ListNode* pcur = prev->next;
-//	int count = 1;
-//	while (pcur->next != pcur)
-//	{
-//		if (m == count)
-//		{
-//			prev->next = pcur->next;
-//			free(pcur);
-//			pcur = prev->next;
-//			count = 1;
-//		}
-//		else
-//		{
-//			prev = pcur;
-//			pcur = pcur->next;
-//			count++;
-//		}
-//
-//	}
-//	return pcur->val;
-//}
 
 //#include<iostream>
 //using namespace std;
